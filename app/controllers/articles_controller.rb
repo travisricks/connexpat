3d1addde8d671f9b9ddcd3ea1c220b9caa9f15dc
Travis Ricks <travis@travisricks.com>
2020-02-25 14:08:33 +0900

    add 'create article' form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
+    
     def index
         @articles = Article.all
     end
@@ -6,4 +8,16 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        @article.save
+
+        redirect_to article_path(@article)
+    end
+
 end
