2f402682fef315167933c8a32f76e2ee003e133f
Travis Ricks <travis@travisricks.com>
2020-02-25 23:11:44 +0900

    add authentication feature

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,8 @@
 class TagsController < ApplicationController
     include TagsHelper
 
+    before_action :require_login, only: [:destroy]
+
     def show
         @tag = Tag.find(params[:id])
     end
